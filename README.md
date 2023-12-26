ae076d80c70bda0ab587d5a1a1b56244cfef0772
yoshimuratakuma0 <outofbody20@gmail.com>
2023-12-27 03:07:06 +0900

    :sparkles: Introduce Docker

---
+++ README.md
@@ -0,0 +1,17 @@
+```
+docker-compose build
+```
+
+```
+docker-compose up
+```
+
+Ganache が立ち上がるまで数十秒待ってから
+
+```
+docker-compose run oracle truffle migrate
+```
+
+```
+docker-compose run caller truffle migrate
+```
--- caller/truffle-config.js
+++ caller/truffle-config.js
@@ -69,6 +69,12 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+
+    development: {
+      host: 'ganache', // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -106,7 +112,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.21",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.19",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
